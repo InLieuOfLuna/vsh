02d7d907619e1e630b632210dc3ccbf8e8c38f93
Luna <luna@lunaluna.me>
2021-09-24 22:21:05 +0100

    Moved tf/addons to tf2/tf/addons

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ tar zxf steamcmd_linux.tar.gz
 rm steamcmd_linux.tar.gz
 wget https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6512-linux.tar.gz
 tar xf sourcemod-1.10.0-git6512-linux.tar.gz
-mv addons tf/addons
+mv addons tf2/tf/addons
 rm sourcemod-1.10.0-git6512-linux.tar.gz
 ./steamcmd.sh +login anonymous +force_install_dir ./tf2 +app_update 232250 +quit
 echo "rcon_password \"$1\"" >> tf2/tf/cfg/server.cfg
