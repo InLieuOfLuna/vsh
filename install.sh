6b91ef6fb9a2b398441f799239ad25c38f8e6f3e
Luna <luna@lunaluna.me>
2021-09-24 21:13:16 +0100

    Added sourcemod to auto install

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar zxf steamcmd_linux.tar.gz
 rm steamcmd_linux.tar.gz
+wget https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6512-linux.tar.gz
+tar xf sourcemod-1.10.0-git6512-linux.tar.gz -C tf2/tf/
+rm sourcemod-1.10.0-git6512-linux.tar.gz
 ./steamcmd.sh +login anonymous +force_install_dir ./tf2 +app_update 232250 +quit
 echo "hostname \"World's fastest vsh server\"" > tf2/tf/cfg/server.cfg
 echo "rcon_password \"$1\"" >> tf/cfg/server.cfg
