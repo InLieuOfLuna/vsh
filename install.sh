e3bc7a28840b00133dd16894d108e68935e131a6
Luna <luna@lunaluna.me>
2021-09-24 19:21:49 +0100

    Moved tf to tf2/tf

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar zxf steamcmd_linux.tar.gz
 rm steamcmd_linux.tar.gz
 ./steamcmd.sh +login anonymous +force_install_dir . +app_update 232250 +quit
-echo "hostname \"World's fastest vsh server\"" > tf/cfg/server.cfg
+echo "hostname \"World's fastest vsh server\"" > tf2/tf/cfg/server.cfg
 echo "rcon_password \"$1\"" >> tf/cfg/server.cfg
-echo "sv_contact \"luna@lunaluna.me\"" >> tf/cfg/server.cfg
-echo "mp_timelimit \"30\"" >> tf/cfg/server.cfg
+echo "sv_contact \"luna@lunaluna.me\"" >> tf2/tf/cfg/server.cfg
+echo "mp_timelimit \"30\"" >> tf2/tf/cfg/server.cfg
