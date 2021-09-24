1f824bb15248c795119e0186e9217b4f2af7edf3
Luna <luna@lunaluna.me>
2021-09-24 22:16:37 +0100

    Stopped import

--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@ wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar zxf steamcmd_linux.tar.gz
 rm steamcmd_linux.tar.gz
 wget https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6512-linux.tar.gz
-tar xf sourcemod-1.10.0-git6512-linux.tar.gz -C tf2/tf/
+tar xf sourcemod-1.10.0-git6512-linux.tar.gz
+mv addons tf/addons
 rm sourcemod-1.10.0-git6512-linux.tar.gz
 ./steamcmd.sh +login anonymous +force_install_dir ./tf2 +app_update 232250 +quit
-echo "hostname \"World's fastest vsh server\"" > tf2/tf/cfg/server.cfg
-echo "rcon_password \"$1\"" >> tf/cfg/server.cfg
-echo "sv_contact \"luna@lunaluna.me\"" >> tf2/tf/cfg/server.cfg
-echo "mp_timelimit \"30\"" >> tf2/tf/cfg/server.cfg
+echo "rcon_password \"$1\"" >> tf2/tf/cfg/server.cfg
