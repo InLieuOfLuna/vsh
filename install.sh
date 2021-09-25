df45210dd3ecfe62b6ade50ec31b1e151ebb0e30
Luna <luna@lunaluna.me>
2021-09-25 16:58:44 +0100

    Added hakurei shrine

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ rm steamcmd_linux.tar.gz
 wget https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6512-linux.tar.gz
 tar xf sourcemod-1.10.0-git6512-linux.tar.gz
 cp -r addons tf2/tf
+cp -r cfg tf2/tf
 rm -r addons
+rm -r cfg
 wget https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git1145-linux.tar.gz
 tar xf mmsource-1.11.0-git1145-linux.tar.gz
 cp -r addons tf2/tf
