adc7e8ad64097d69f4f0d65d04f39ab8f6190fba
Luna <luna@lunaluna.me>
2021-09-25 13:04:55 +0100

    Moved tf2items back to installation and removed ff2

--- install.sh
+++ install.sh
@@ -11,5 +11,9 @@ tar xf mmsource-1.11.0-git1145-linux.tar.gz
 cp -r addons tf2/tf
 rm -r addons
 rm sourcemod-1.10.0-git6512-linux.tar.gz
+wget https://builds.limetech.io/files/tf2items-1.6.4-hg279-linux.zip
+unzip tf2items-1.6.4-hg279-linux.zip
+cp -rn addons tf2/tf
+rm -r addons
 ./steamcmd.sh +login anonymous +force_install_dir ./tf2 +app_update 232250 +quit
 echo "rcon_password \"$1\"" >> tf2/tf/cfg/server.cfg
